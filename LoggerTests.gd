52956c24d85152168e4a3c9d31cb943322ba1db8
SirMoM <pirat99@online.de>
2020-12-18 13:39:51 +0100

    Init testsuit. with custom logger
    This should use the https://github.com/KOBUGE-Games/godot-logger
    enentually!

---
+++ LoggerTests.gd
@@ -0,0 +1,144 @@
+extends VBoxContainer
+
+export (String) var file_path_for_logfile_module = "res://LoggerTests.log"
+
+const debug_table = """level	|verbose	|debug	|info	|warn	|error	|
+verbose|	✓	|	✓	|	✓	|	✓	|	✓	|
+debug  |	X	|	✓	|	✓	|	✓	|	✓	|
+info   |	✗X	|	X	|	✓	|	✓	|	✓	|
+warn   |	X	|	X	|	X	|	✓	|	✓	|
+error  |	X	|	X	|	X	|	x	|	✓	|"""
+
+
+onready var feedback_label = $RichTextLabel
+
+var standart_module
+var named_module
+var logfile_module
+
+
+func _ready() -> void:
+	standart_module = Logger.get_module()
+	
+	named_module = Logger.get_module("App")
+	
+	logfile_module = Logger.get_module("FILE")
+	logfile_module.external_sink = Logger.Logfile.new("res://logs/Logger.log")
+	logfile_module.output_strategies = [Logger.STRATEGY_EXTERNAL_SINK,
+	Logger.STRATEGY_EXTERNAL_SINK, Logger.STRATEGY_EXTERNAL_SINK,
+	Logger.STRATEGY_EXTERNAL_SINK, Logger.STRATEGY_EXTERNAL_SINK]
+	print()
+	print("##################################################################")
+	print()
+
+func test_cases(_loglevel, _module = null):
+	if _module:
+		Logger.get_module(_module).output_level = _loglevel
+	else:
+		Logger.get_module().output_level = _loglevel
+	
+	test_all_levels(_module)
+	print("##################################################################")
+	print()
+
+
+func test_all_levels(_module):
+	if _module:
+		Logger.verbose("Verbose log!", _module)
+		Logger.debug("Debug log!", _module)
+		Logger.info("Info log!", _module)
+		Logger.warn("Warn log!", _module)
+		Logger.error("Error log!", _module)
+	else: 
+		Logger.verbose("Verbose log!")
+		Logger.debug("Debug log!")
+		Logger.info("Info log!")
+		Logger.warn("Warn log!")
+		Logger.error("Error log!")
+
+func _on_TestsVerbose_pressed(module) -> void:
+	var module_name
+	if module == 0:
+		test_cases(Logger.VERBOSE)
+		module_name = "Main"
+	if module == 1:
+		test_cases(Logger.VERBOSE, "App")
+		module_name = "App"
+	if module == 2:
+		test_cases(Logger.VERBOSE, "FILE")
+		module_name = "FILE"
+		
+	feedback_label.text += "%s verbose test done!\n" % module_name# + debug_table 
+	
+
+
+func _on_TestsDebug_pressed(module) -> void:
+	var module_name
+	if module == 0:
+		test_cases(Logger.DEBUG)
+		module_name = "Main"
+	if module == 1:
+		test_cases(Logger.DEBUG, "App")
+		module_name = "App"
+	if module == 2:
+		test_cases(Logger.DEBUG, "FILE")
+		module_name = "FILE"
+	feedback_label.text += "%s debug test done!\n" % module_name# + debug_table 
+
+func _on_TestsInfo_pressed(module) -> void:
+	var module_name
+	if module == 0:
+		test_cases(Logger.INFO)
+		module_name = "Main"
+	if module == 1:
+		test_cases(Logger.INFO, "App")
+		module_name = "App"
+	if module == 2:
+		test_cases(Logger.INFO, "FILE")
+		module_name = "FILE"
+	feedback_label.text += "%s info test done!\n"% module_name# + debug_table 
+
+func _on_TestsWarn_pressed(module) -> void:
+	var module_name
+	if module == 0:
+		test_cases(Logger.WARN)
+		module_name = "Main"
+	if module == 1:
+		test_cases(Logger.WARN, "App")
+		module_name = "App"
+	if module == 2:
+		test_cases(Logger.WARN, "FILE")
+		module_name = "FILE"
+	feedback_label.text += "%s warn test done!\n"% module_name# + debug_table 
+
+
+func _on_TestsError_pressed(module) -> void:
+	var module_name
+	if module == 0:
+		test_cases(Logger.ERROR)
+		module_name = "Main"
+	if module == 1:
+		test_cases(Logger.ERROR, "App")
+		module_name = "App"
+	if module == 2:
+		test_cases(Logger.ERROR, "FILE")
+		module_name = "FILE"
+	feedback_label.text += "%s error test done!\n" % module_name# + debug_table 
+
+
+func _on_AutomaticTest_pressed() -> void:
+	for level in range(0,5):
+		test_cases(level)
+		feedback_label.text += "Main %s test done!\n" % Logger.LEVELS[level]# + debug_table
+	feedback_label.text += "All main tests done!\n\n"
+	
+	for level in range(0,5):
+		test_cases(level, "App")
+		feedback_label.text += "App %s test done!\n" % Logger.LEVELS[level]
+	feedback_label.text += "All app tests done!\n\n"
+	
+	for level in range(0,5):
+		test_cases(level, "FILE")
+		feedback_label.text += "App %s test done!\n" % Logger.LEVELS[level]
+	feedback_label.text += "All FILE tests done!\n\n"
+		
